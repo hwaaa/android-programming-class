43baefb6d8271c4a6423e0b8603a1c8418a70c44
hwaaa <hshwaa@gmail.com>
2018-12-14 13:14:14 +0900

    Seekbar 실습

---
+++ Seekbar/app/src/main/java/hwa/helloworld/seekbar/MainActivity.java
@@ -0,0 +1,40 @@
+package hwa.helloworld.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar, seekBar2, seekBar3;
+    TextView tv_vol;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar2 = (SeekBar)findViewById(R.id.seekBar2);
+        seekBar3 = (SeekBar)findViewById(R.id.seekBar3);
+        tv_vol = (TextView)findViewById(R.id.tv_vol);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv_vol.setText("vol : " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
